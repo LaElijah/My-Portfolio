ba7bb62401b3af7a5c2cd9d8103e82f118b10486
Lloyd Elijah Allotey <elijah.allotey.w@gmail.com>
2023-10-04 22:00:35 +0000

    wip: updated hero section, finishing skills and socials area, branching

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -11,23 +11,33 @@ export default function Hero() {
     return (
         <section className={styles.container}>
 
-            <div className={styles.details}>
 
-                <h2>Hi! my name is,</h2>
-                <h1>Elijah Allotey,</h1>
-                <h3>Developing solutions on the web!</h3>
+            <div className={styles.hook}>
 
-                <button className={styles.button}>Contact me</button>
+
+                <div className={styles.details}>
+
+                    <h2>Hi! my name is,</h2>
+                    <h1>Elijah Allotey,</h1>
+                    <h3>Developing solutions on the web!</h3>
+
+                    <button className={styles.button}><p>Contact me</p></button>
+                </div>
+
+                <div className={styles.socials}>
+                    socials
+                </div>
+
             </div>
 
-            <div className={styles.skills}>
-                skills
-            </div>
+            <div className={styles.media}>
 
 
-            <div className={styles.image}>
+                <div className={styles.image}>
 
-                <img src={"/assets/bust.png"} />
+                    <img src={"/assets/bust.png"} />
+
+                </div>
 
             </div>
 
