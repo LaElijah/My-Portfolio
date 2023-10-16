b1d051bb0aab0a62bf6f6f3fd9a4a04b0cb63a00
laelijah <laelijah@Ubuntu.laelijah.dev>
2023-10-15 22:44:09 -0500

    feature: extended animate presence feautre to motion renderer

--- src/app/_components/elements/motionRenderer.tsx
+++ src/app/_components/elements/motionRenderer.tsx
@@ -1,6 +1,24 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence, AnimatePresenceProps } from "framer-motion";
 
-export default function MotionRenderer({ children, ...options }: any) {
-  return <motion.div {...options}>{children}</motion.div>;
+type CommonProps = any;
+
+type MotionRendererProps = CommonProps & {
+  children: JSX.Element | JSX.Element[];
+  animatePresence?: AnimatePresenceProps;
+};
+export default function MotionRenderer({
+  children,
+  animatePresence,
+  ...options
+}: MotionRendererProps) {
+  if (animatePresence) {
+    return (
+      <AnimatePresence {...animatePresence}>
+        <motion.div {...options}>{children}</motion.div>
+      </AnimatePresence>
+    );
+  } else {
+    return <motion.div {...options}>{children}</motion.div>;
+  }
 }
